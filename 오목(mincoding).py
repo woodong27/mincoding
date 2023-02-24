6b092bcc508fe95974405f33beed58db94483a79
woodong27 <cdw5548@gmail.com>
2023-02-24 17:38:35 +0900

    02/24/2023 오목 다 못품

--- 오목(mincoding).py
+++ 오목(mincoding).py
@@ -1,36 +1,27 @@
 board=[list(map(int,input().split()))for _ in range(19)]
-from pprint import pprint
 
 di=[-1,1,0,0,-1,-1,1,1]
 dj=[0,0,-1,1,-1,1,-1,1]
-pprint(board)
 
-ans=0
-winp=[]
+wincolor=0
 for i in range(19):
     for j in range(19):
         if board[i][j]!=0:
-            color=board[i][j]
-            print(i,j)
-            cnt=0
             for k in range(8):
-                for m in range(1,7):
-                    ni=i+di[k]*m
-                    nj=j+dj[k]*m
-                    if 0<=ni<19 and 0<=nj<19:
-                        if board[ni][nj]==color:
-                            cnt+=1
-                        else:
-                            break
+                position = [(i, j)]
+                m=1
+                while 0<=i+di[k]*m<19 and 0<=j+dj[k]*m<19 and board[i][j]==board[i+di[k]*m][j+dj[k]*m]:
+                    m+=1
 
-            if cnt==5:
-                ans+=1
+                if m==5:
 
-                print(i,j)
-                winp.append((i+1,j+1))
+                    wincolor=board[i][j]
+                    position.append((i+di[k]*m,j+dj[k]*m))
+                    ansp=position
 
-print(ans)
-print(winp[0][0],winp[0][1])
+print(wincolor)
+print(ansp)
+
 
 
 
